110b4c2f3f3d6f518606411da07ff3ed605f85c7
afbaviv <vkalgathra@afba.com>
2020-07-31 20:09:01 +0530

    Settings whitelist corrected to not include path.

--- cap_ui_tool/cap_ui_tool/settings.py
+++ cap_ui_tool/cap_ui_tool/settings.py
@@ -140,6 +140,6 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000/',
-    'http://127.0.0.1:8000/'
+    'http://localhost:8000',
+    'http://127.0.0.1:8000'
 )
